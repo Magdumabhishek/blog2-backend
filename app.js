a969dd290e367c606b87f5b5d45a088cf339fa87
Magdumabhishek <129311187+Magdumabhishek@users.noreply.github.com>
2024-11-24 15:52:59 +0530

    added cors

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose")
 const express = require("express")
 const userRoute = require("./route/userRoute")
 const blogRoute = require("./route/blogRoute")
+const cors = require("cors")
 require("dotenv/config")
 
 const app = express()
 app.use(express.json())
+app.use(cors)
 
 app.get("/",(req,res)=>{
     res.send("Home")
--- controller/userController.js
+++ controller/userController.js
@@ -17,7 +17,7 @@ exports.postUser = async (req,res) => {
         if (userExist) return res.status(500).json({errors:true,message:"User Already Exists"})
             req.body.password = await bcrypt.hash(req.body.password,10)
         const data = await User.create(req.body)
-        return res.json({errors:false,data:data})
+        return res.json({errors:false,message:"User Created Succefully"})
     } catch (error) {
         return res.status(400).json({errors:true,message:error.message})
     }
@@ -26,7 +26,7 @@ exports.postUser = async (req,res) => {
 exports.updateUser = async (req,res) => {
     try {
         const data = await User.findByIdAndUpdate(req.params.id,req.body,{new:true})
-        return res.json({errors:true,data:data})
+        return res.json({errors:true,message:"data updated Successfully"})
     } catch (error) {
         return res.status(400).json({errors:true,message:error.message})
     }
@@ -35,7 +35,7 @@ exports.updateUser = async (req,res) => {
 exports.deleteUser = async (req,res) => {
     try {
         const data = await User.findByIdAndDelete(req.params.id)
-        return res.json({errors:false,data:data})
+        return res.json({errors:false,message:"User Deleted Successfully"})
     } catch (error) {
         return res.status(400).json({error:true,message:error.messasge})
     }
@@ -49,7 +49,7 @@ exports.login = async (req,res) => {
 
         if(!comparePassword) return res.status(400).json({errors:true,message:"invalid email or password"})
             const token = await jwt.sign({id:userExist._id},process.env.SEC)
-            return res.json({errors:false,data:{token:token,data:data}}) 
+            return res.json({errors:false,data:{token:token,message:"Logged in Successfully"}}) 
         }
         catch (error) {
             return res.status(400).json({errors:false,message:error.message})
